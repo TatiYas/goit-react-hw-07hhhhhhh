f21bc9c7ab9765ba74ab77907ca1fb385ee61360
TatiYas <iasinovska_lawyer@ukr.net>
2024-08-11 22:34:56 +0200

    Update_5

--- src/main.jsx
+++ src/main.jsx
@@ -20,11 +20,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 
 
 
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>
-  );
